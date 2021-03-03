59a7eb46ed5ac72defcedb2eb4f6d8514f04faff
hilario-alberto <hilario492@gmail.com>
2021-03-02 18:14:34 -0600

    draw points and add to list
    this section draw points and identificates if the point is with the left click or with rigth click.
    Finally add the points to a list (points).      for AG

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,34 +30,62 @@ namespace P1_Perceptron
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(806, 93);
+            this.button1.Location = new System.Drawing.Point(700, 54);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(185, 55);
             this.button1.TabIndex = 0;
             this.button1.Text = "INICIALIZAR";
             this.button1.UseVisualStyleBackColor = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Da click sobre el recuadro blanco para crear objetos.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(60, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(496, 448);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1130, 776);
+            this.ClientSize = new System.Drawing.Size(934, 585);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,40 @@ namespace P1_Perceptron
 {
     public partial class Form1 : Form
     {
+
+        int cont = 0;
+        List<Point> points = new List<Point>();
+        Bitmap bmp;
         public Form1()
         {
             InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            points.Add(e.Location);
+            Graphics g = Graphics.FromImage(bmp);
+            Brush b1 = new SolidBrush(Color.Red);
+            Brush b2 = new SolidBrush(Color.Blue);
+
+            if (e.Button == MouseButtons.Right)
+            {
+                g.FillEllipse(b1, e.X - 2, e.Y - 2, 10, 10);
+            }
+            else
+            {
+                g.FillEllipse(b2, e.X - 2, e.Y - 2, 10, 10);
+            }
+
+            pictureBox1.Image = bmp;
+            pictureBox1.Refresh();
         }
     }
 }
